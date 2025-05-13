ce1c1ff282ad9a2783a74d55599ff9f4843269fc
Sun43-3rd <157233621+Sun43-3rd@users.noreply.github.com>
2025-05-13 17:34:12 -0400

    Update ReadArchive.js

--- ReadArchive.js
+++ ReadArchive.js
@@ -1,5 +1,3 @@
-import * as Tools from '/SearchNY_Lottery/AnalysisTools1.js'
-
 import * as DATANY3 from '/SearchNY_Lottery/NY3.js'
 import * as DATANY4 from '/SearchNY_Lottery/NY4.js'
 
@@ -8,12 +6,6 @@ function Daily_Array(type, interger, raw){
     let result = [];
     (type == 3 || type == 4) ? result = Object.values(raw[interger]).filter((x, i) => i > 0).map((x) => x.split('')) : 
     type == 10 ? result = [raw[interger]['winning_numbers']]: "INVALID"
-    return result;
-}
-
-function False_Array(drawing, start, end, text_true){
-    const outcomes = Tools.Get_Outcomes(start, end, text_true)
-    const result = outcomes.filter((x) => drawing.includes(x) !== true)
     return result;
 }
 
@@ -37,8 +29,8 @@ function Extract_Data(type, size, raw, sheets = false){
             else if(type == 10){for(let i = 0; i <= size; i++){holder.push(Seperate(Daily_Array(type, i, raw)[0], 2))}}
     return holder.flat();}
 
-const Pick3_Raw = [[await DATANY3.NY3LIVE, false]].map((x) => Extract_Data(3, Object.values(x[0]).length - 1, x[0], x[1])).map((x) => [x, x.map((y) => False_Array(y, 0, 9, false)), x.map((y) => Tools.R_Duplicate(y)), x.map((y) => Tools.R_Duplicate(False_Array(y, 0, 9, false)))])
-const Pick4_Raw = [[await DATANY4.NY4LIVE, false]].map((x) => Extract_Data(4, Object.values(x[0]).length - 1, x[0], x[1])).map((x) => [x, x.map((y) => False_Array(y, 0, 9, false)), x.map((y) => Tools.R_Duplicate(y)), x.map((y) => Tools.R_Duplicate(False_Array(y, 0, 9, false)))])
+const Pick3_Raw = [[await DATANY3.NY3LIVE, false]].map((x) => Extract_Data(3, Object.values(x[0]).length - 1, x[0], x[1]))
+const Pick4_Raw = [[await DATANY4.NY4LIVE, false]].map((x) => Extract_Data(4, Object.values(x[0]).length - 1, x[0], x[1]))
 
 export let NY3_Drawings = [await DATANY3.NY3LIVE, Pick3_Raw[0]];
 export let NY4_Drawings = [await DATANY4.NY4LIVE, Pick4_Raw[0]];
