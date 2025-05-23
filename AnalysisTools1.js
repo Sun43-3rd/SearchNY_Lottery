863793fc01e92261a76b273c0d1e1ccad88d79a4
Sun43-3rd <learningcentral37@gmail.com>
2025-05-23 13:00:34 -0400

    updated for search date command

--- AnalysisTools1.js
+++ AnalysisTools1.js
@@ -19,9 +19,6 @@ export function Match_ArExact(x, y){
     return (x.every((a, b) => a === y[b]))
 }
 
-export function Q_Size(size, y1, y2){
-    return (size == 3 ? y1 : size == 4 ? y2: undefined)
-}
 
 export function Box_S2(subject, pos_1, end_pos, select){
     const result = subject.filter((x) => x.slice(pos_1, end_pos).sort((a, b) => a - b).join('') === select.sort((a, b) => a - b).join(''));
--- Search.js
+++ Search.js
@@ -1,30 +1,33 @@
 import * as Tools from '/SearchNY_Lottery/AnalysisTools1.js'
 import * as Arch from '/SearchNY_Lottery/ReadArchive.js'
 
-
-function Get_Dates(x, y){return x.filter((x) => x[Tools.Q_Size(y, 'evening_daily','evening_win_4')] !== undefined).map((x) => x[Tools.Q_Size(y, 'midday_daily','midday_win_4')] !== undefined? [[new Date(x['draw_date']), 'Evening'], [new Date(x['draw_date']), 'Midday']] : [[new Date(x['draw_date']), 'Evening']]).flat().map((x) => [x[0].toLocaleDateString(undefined, options), x[1]])
-}
-
 const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
 
-const regex = /^(\w+),\s+(\w+)\s+(\d+),\s+(\d{4})$/;
+function Get_Dates(x, y){return x.filter((x) => x[(y == 3 ? 'evening_daily':'evening_win_4')] !== undefined).map((x) => x[(y == 3 ? 'midday_daily':'midday_win_4')] !== undefined? [[new Date(x['draw_date']), 'Evening'], [new Date(x['draw_date']), 'Midday']] : [[new Date(x['draw_date']), 'Evening']]).flat().map((x) => [x[0].toLocaleDateString('en-US', options), x[1]])
+}
 
 
 function SetUp(){
     const game = document.getElementById("Select-Game"); const b_check = document.getElementById("Box"); const input = document.getElementById("input"); const table = document.getElementById('table'); const t_header = document.getElementById('T-Header');
-    let results = [];
+    const symbol = document.getElementById('sort'); let results = [];
 
     input.addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
-            const drawing = (game.value == '3' ? Arch.NY3_Drawings: Arch.NY4_Drawings)
-            const date = (game.value == '3' ? Get_Dates(drawing[0], game.value) : Get_Dates(drawing[0], game.value));
-            const data = drawing[1].map((x, i) => [date[i], x, Tools.Box_C(x)])
-            let search = input.value.split('')
-            console.log(search)
+            const drawing = (game.value == '3' ? Arch.NY3_Drawings: game.value == '4' ? Arch.NY4_Drawings : isNaN())
+            const date = (game.value == '3' || game.value == '4' ? Get_Dates(drawing[0], game.value) : isNaN());
+            const data = (game.value == '3' || game.value == '4' ? drawing[1].map((x, i) => [date[i], x, Tools.Box_C(x)]) : isNaN())
+            let search = input.value;
             
-            b_check.checked === true? results = Tools.Box_BCode(data, 2, search) : results = data.filter((x) => Tools.Match_ArExact(x[1], search));
-            console.log(results)
 
+            if((search.search(/[A-Za-z]/) !== '!' && search.search(/[A-Za-z]/) !== -1))
+                {results = data.filter((x) => x[0].join(',').toLowerCase().includes(search.toLowerCase())); 
+            }
+            else{
+            search = search.split('')
+            b_check.checked === true? results = Tools.Box_BCode(data, 2, search) : results = data.filter((x) => Tools.Match_ArExact(x[1], search))
+            };
+            
+            
            Array.prototype.filter.call(table.children, (x) => x.id !== 'T-Header' && x.id !== 'NA').map((x) => x.remove())
             
             if(results.length === 0){
@@ -39,34 +42,40 @@ function SetUp(){
            else{
                Array.prototype.filter.call(table.children, (x) => x.id !== 'T-Header').map((x) => x.remove());
             
-            results = results.map((x) => [x, x[0][0].split(regex).concat(x[0][1], x[1].join('')).filter((x) => x !== '')]);
-
-            results.map((x) => 
+            
+               results.map((x) => 
                 {const tr = document.createElement('tr'); 
-                    tr.date = new Date(x[0][0][0]); tr.Weekday = tr.date.getDay(); tr.Month = tr.date.getMonth(); tr.Day = tr.date.getDate(); tr.Year = tr.date.getYear(); tr.Time = ['Midday', 'Evening'].indexOf(x[0][0][1]); tr.Result = Number(x[0][1].join(''))
-                     x[1].map((y) => {const td = document.createElement('td'); td.className = 't-data';
-                        ; td.textContent = y; tr.appendChild(td)}); table.appendChild(tr)})
+                    tr.date = new Date(x[0][0]); 
+                    tr.Weekday = [tr.date.getDay(), Intl.DateTimeFormat('en-US', {'weekday': 'long'}).format(tr.date)]; tr.Month = [tr.date.getMonth(), Intl.DateTimeFormat('en-US', {'month': 'long'}).format(tr.date)]; tr.Day = [tr.date.getDate(), Intl.DateTimeFormat('en-US', {'day': 'numeric'}).format(tr.date)]; tr.Year = [tr.date.getYear(), Intl.DateTimeFormat('en-US', {'year': 'numeric'}).format(tr.date)]; tr.Time = [['Midday', 'Evening'].indexOf(x[0][1]), x[0][1]]; tr.Result = [Number(x[1].join('')), x[1].join('')];
+                     [tr.Weekday, tr.Month, tr.Day, tr.Year, tr.Time, tr.Result].map((x) => x[1]).map((y) => {const td = document.createElement('td'); td.className = 't-data';
+                    td.textContent = y; tr.appendChild(td)}); table.appendChild(tr)})
                      }
-        }})
+                     
+                     // Erase Sort // 
+                     symbol.innerHTML === '' 
+                     Array.prototype.map.call(t_header.children, (x) => x.style.color = 'antiquewhite')
+            
+            }
+        })
 
          
         function Sort_By(column){
             const rows = Array.prototype.filter.call(table.children,(x) => x !== t_header);
             const up = String.fromCharCode(8593); const down = String.fromCharCode(8595)
             const sort_by = column.firstElementChild.innerHTML.trimEnd(); 
-            const symbol = document.getElementById('sort');
+            
             Array.prototype.map.call(t_header.children, (x) => x.style.color = 'antiquewhite')
            
             if(symbol.innerHTML === '' || column.lastElementChild.id !== 'sort'){
-            symbol.innerHTML = up;  column.style.color = 'black'; column.appendChild(symbol);
+            symbol.innerHTML = up;  column.style.color = 'silver'; column.appendChild(symbol);
        
-            Array.prototype.sort.call(rows, (a, b) => a?.[sort_by] - b?.[sort_by])
+            Array.prototype.sort.call(rows, (a, b) => a?.[sort_by][0] - b?.[sort_by][0])
             table.replaceChildren(t_header, ...rows)
             
            } else if(symbol.innerHTML === up){
-            symbol.innerHTML = down; column.style.color = 'black'; column.appendChild(symbol);
+            symbol.innerHTML = down; column.style.color = 'silver'; column.appendChild(symbol);
 
-            Array.prototype.sort.call(rows, (a, b) => b?.[sort_by] - a?.[sort_by])
+            Array.prototype.sort.call(rows, (a, b) => b?.[sort_by][0] - a?.[sort_by][0])
             table.replaceChildren(t_header, ...rows)
             
             
