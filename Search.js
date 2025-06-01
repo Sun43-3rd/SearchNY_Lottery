11eac71bd2af013dbef4c7dc459ec063fcc2cdaa
Sun43-3rd <157233621+Sun43-3rd@users.noreply.github.com>
2025-06-01 19:19:08 -0400

    Update Search.js

--- Search.js
+++ Search.js
@@ -23,7 +23,7 @@ function SetUp(){
 
             else{
                 search = search.split('')
-                b_check.checked === true? results = Tools.Box_BCode(data, 2, search) : results = (search.length < game.value ? data.filter((x) => x[1].join('').includes(search.join(''))) : data.filter((x) => Match_ArExact(x[1], search)))
+                b_check.checked === true? results = Tools.Box_BCode(data, 2, search) : results = (search.length < game.value ? data.filter((x) => x[1].join('').includes(search.join(''))) : data.filter((x) => Tools.Match_ArExact(x[1], search)))
             };
             
            Array.prototype.filter.call(table.children, (x) => x.id !== 'T-Header' && x.id !== 'NA').map((x) => x.remove())
