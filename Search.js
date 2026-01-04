0f4a7a8073611c91c932dd24b24bca9c47fbc686
Sun43-3rd <learningcentral37@gmail.com>
2026-01-03 22:01:01 -0500

    added saved table other changes pending

--- Search.js
+++ Search.js
@@ -7,7 +7,11 @@ function SetUp(){
     function Get_Dates(x, y){return x.filter((x) => x[(y == 3 ? 'evening_daily':'evening_win_4')] !== undefined).map((x) => x[(y == 3 ? 'midday_daily':'midday_win_4')] !== undefined? [[new Date(x['draw_date']), 'Evening'], [new Date(x['draw_date']), 'Midday']] : [[new Date(x['draw_date']), 'Evening']]).flat().map((x) => [x[0].toLocaleDateString('en-US', options), x[1]])
     }
 
-    const game = document.getElementById("Select-Game"); const b_check = document.getElementById("Box"); const input = document.getElementById("input"); const submit = document.getElementById("Submit"); const table = document.getElementById('table'); const t_header = document.getElementById('T-Header');
+    const game = document.getElementById("Select-Game"); const b_check = document.getElementById("Box"); const input = document.getElementById("input"); const submit = document.getElementById("Submit"); 
+    const result_table = document.getElementById('result-table'); 
+    const result_t_header = document.getElementById('result-table-Header');
+    const saved_table = document.getElementById('saved-table');
+    const saved_table_header = document.getElementById('saved-table-Header');
     const symbol = document.getElementById('sort'); let results = [];
 
     function Search_NY(){
@@ -23,6 +27,7 @@ function SetUp(){
         const date = (game.value == '3' || game.value == '4' ? Get_Dates(drawing[0], game.value) : isNaN());
         const data = (game.value == '3' || game.value == '4' ? drawing[1].map((x, i) => [date[i], x, Tools.Box_C(x)]) : isNaN())
         
+        console.log(data), console.log(date), console.log(drawing);
         // If Search isn't a searchable value, default to This Years Drawings 
         search = (search.search(/[A-Za-z]/) === -1 && search.search(/[0-9]/) === -1) ? 'date 2025' : search 
         
@@ -51,36 +56,73 @@ function SetUp(){
                     }
             }
                 
-            else{
-                search = search.split('')
+            else{ // Number Search
+
+                search = search.split('');
                 b_check.checked === true? results = Tools.Box_BCode(data, 2, search) : results = (search.length < game.value ? data.filter((x) => x[1].join('').includes(search.join(''))) : data.filter((x) => Tools.Match_ArExact(x[1], search)))
             };
-            
-           Array.prototype.filter.call(table.children, (x) => x.id !== 'T-Header' && x.id !== 'NA').map((x) => x.remove())
+        
+        // Erase Past Results
+        Array.prototype.filter.call(result_table.children, (x) => x.id !== 'result-table-Header' && x.id !== 'NA').map((x) => x.remove())
             
             if(results.length === 0){
                 const span = document.createElement('span'); span.id ='NA'; span.innerHTML = 'N/A'; span.style.fontSize = '24pt'; 
                 
-                if(Array.prototype.find.call(table.children, (x) => x.id === 'NA') !== undefined){  
+                if(Array.prototype.find.call(result_table.children, (x) => x.id === 'NA') !== undefined){  
                     document.getElementById('NA').toggleAttribute('hidden')
                     setTimeout(() => {document.getElementById('NA').toggleAttribute('hidden');}, 500);
                     }
-                    else{table.appendChild(span)}
+                    else{result_table.appendChild(span)}
                 }
 
            else{
-               Array.prototype.filter.call(table.children, (x) => x.id !== 'T-Header').map((x) => x.remove());
+               Array.prototype.filter.call(result_table.children, (x) => x.id !== 'result-table-Header').map((x) => x.remove());
                
-               results.map((x) => 
-                {const tr = document.createElement('tr'); 
-                    tr.date = new Date(x[0][0]); 
-                    tr.Weekday = [tr.date.getDay(), Intl.DateTimeFormat('en-US', {'weekday': 'long'}).format(tr.date)]; tr.Month = [tr.date.getMonth(), Intl.DateTimeFormat('en-US', {'month': 'long'}).format(tr.date)]; tr.Day = [tr.date.getDate(), Intl.DateTimeFormat('en-US', {'day': 'numeric'}).format(tr.date)]; tr.Year = [tr.date.getYear(), Intl.DateTimeFormat('en-US', {'year': 'numeric'}).format(tr.date)]; tr.Time = [['Midday', 'Evening'].indexOf(x[0][1]), x[0][1]]; tr.Result = [Number(x[1].join('')), x[1].join('')];
-                     [tr.Weekday, tr.Month, tr.Day, tr.Year, tr.Time, tr.Result].map((x) => x[1]).map((y) => {const td = document.createElement('td'); td.className = 't-data';
-                    td.textContent = y; tr.appendChild(td)}); table.appendChild(tr)})
+                results.map((x) => 
+                    {
+                        const tr = document.createElement('tr');  tr.copy = '';// copy placeholder for proper deletion
+                       
+                        // Create and Add Save Checkbox
+                        const option = document.createElement('td'); const save_checkbox = document.createElement('input'); save_checkbox.type = 'checkbox';
+                        option.appendChild(save_checkbox); tr.appendChild(option);
+
+                        save_checkbox.addEventListener('click', () => {
+                            
+                            // Add Copy when Checked and remove Copy when checked is false
+                            if(save_checkbox.checked){
+                                // Create Copy 
+                                let copy_tr = tr.cloneNode(true); copy_tr.removeChild(copy_tr.firstChild); copy_tr.className = 'copy';
+                                Object.assign(copy_tr, tr)
+                               
+                                tr.copy = copy_tr
+                                saved_table.appendChild(copy_tr)
+                                Array.prototype.map.call(tr.children, (x) => {x.style.color = 'grey'})
+                            }
+                            if(!save_checkbox.checked){
+                                Array.prototype.map.call(tr.children, (x) => {x.style.color = 'white'})
+                                tr.copy.remove()
+                                
+                            }
+
+                            
+                        })
+
+                        // Get and Add Data to Rows
+                        tr.date = new Date(x[0][0]); 
+                        tr.Weekday = [tr.date.getDay(), Intl.DateTimeFormat('en-US', {'weekday': 'long'}).format(tr.date)]; tr.Month = [tr.date.getMonth(), Intl.DateTimeFormat('en-US', {'month': 'long'}).format(tr.date)]; tr.Day = [tr.date.getDate(), Intl.DateTimeFormat('en-US', {'day': 'numeric'}).format(tr.date)]; tr.Year = [tr.date.getYear(), Intl.DateTimeFormat('en-US', {'year': 'numeric'}).format(tr.date)]; tr.Time = [['Midday', 'Evening'].indexOf(x[0][1]), x[0][1]]; tr.Result = [Number(x[1].join('')), x[1].join('')];
+                     
+                        [tr.Weekday, tr.Month, tr.Day, tr.Year, tr.Time, tr.Result].map((x) => x[1]).map((y) => {
+                            const td = document.createElement('td'); td.className = 't-data';
+                            td.textContent = y; tr.appendChild(td)
+                            }); 
+
+                        result_table.appendChild(tr)
+                    })
                }
-                     // Erase Sort // 
-                symbol.innerHTML === '' 
-                Array.prototype.map.call(t_header.children, (x) => x.style.color = 'white')
+
+        // Erase Sort 
+        symbol.innerHTML === '' 
+        Array.prototype.map.call(result_t_header.children, (x) => x.style.color = 'white')
         
         // Enable Input and Submit Button
         input.disabled = false; submit.disabled = false;
@@ -111,17 +153,18 @@ function SetUp(){
     }
     
     function Sort_By(column){
-        const rows = Array.prototype.filter.call(table.children,(x) => x !== t_header);
+        
+        const rows = Array.prototype.filter.call(column.parentElement.parentElement.children,(x) => x !== column.parentElement);
         const up = String.fromCharCode(8593); const down = String.fromCharCode(8595)
-        const sort_by = column.firstElementChild.innerHTML.trimEnd(); 
-            
-        Array.prototype.map.call(t_header.children, (x) => x.style.color = 'white')
-           
+        const sort_by = column.firstElementChild.innerHTML.trim(); 
+      
+        Array.prototype.map.call(column.parentElement.children, (x) => x.style.color = 'white')
+           console.log(rows), console.log(sort_by)
         if(symbol.innerHTML === '' || column.lastElementChild.id !== 'sort'){
             symbol.innerHTML = up;  column.style.color = 'silver'; column.appendChild(symbol);
        
             Array.prototype.sort.call(rows, (a, b) => a?.[sort_by][0] - b?.[sort_by][0])
-            table.replaceChildren(t_header, ...rows)
+            column.parentElement.parentElement.replaceChildren(column.parentElement, ...rows)
             
         } 
 
@@ -129,7 +172,7 @@ function SetUp(){
             symbol.innerHTML = down; column.style.color = 'silver'; column.appendChild(symbol);
 
             Array.prototype.sort.call(rows, (a, b) => b?.[sort_by][0] - a?.[sort_by][0])
-            table.replaceChildren(t_header, ...rows)
+            column.parentElement.parentElement.replaceChildren(column.parentElement, ...rows)
                 
         } 
 
@@ -137,12 +180,12 @@ function SetUp(){
             symbol.innerHTML = ''; column.style.color = 'white'; column.appendChild(symbol);
           
             Array.prototype.sort.call(rows, (a, b) => b.date - a.date)
-            table.replaceChildren(t_header, ...rows) 
+            column.parentElement.parentElement.replaceChildren(column.parentElement, ...rows) 
           
         }  
             
     }
-        
+        // Event Listeners Throughout
         input.addEventListener('keydown', (event) => {if(event.key === 'Enter'){
                 Search_NY()                                       
             }
@@ -152,7 +195,8 @@ function SetUp(){
         // input focus automatic Search
         input.addEventListener('focus', (event) => { Search_NY.Timed(15) })
         window.addEventListener('click', (event) => { Array.prototype.map.call(document.querySelector('.Wrapper-0-Infos').children, (x) => {x.classList.contains('show') ? x.classList.remove('show') : x}) })
-        Array.prototype.map.call(t_header.children, ((x) => x.onclick = () => { Sort_By(x) }))
+        Array.prototype.map.call(result_t_header.children, ((x) => x.onclick = () => { Sort_By(x) }))
+        Array.prototype.map.call(saved_table_header.children, ((x) => x.onclick = () => { Sort_By(x) }))
         input.focus()
 
         
